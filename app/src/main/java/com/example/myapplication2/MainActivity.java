d0cae02635a92678500109545e4c3b71337ca23e
Taelot <sdsqianxrfxdho@163.com>
2022-10-24 02:24:05 +1100

    Finished UI setup
    TODO: implementing method for checkingFilled + submit
    TODO: Location

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication2;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -30,6 +31,8 @@ import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.firestore.Source;
 import com.google.firebase.firestore.Transaction;
 import com.google.firebase.firestore.WriteBatch;
+
+import android.content.DialogInterface;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -79,31 +82,74 @@ import java.util.concurrent.TimeUnit;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private Button submitButton;
+    //private TextView alertView;
+    private EditText activityName;
+    private EditText activityDescription;
+    private EditText activityLocation;
+    private EditText activityStartTime;
+    private EditText activityEndTime;
+    private EditText activityTag;
+    private EditText activityMaxPeople;
+    private EditText activityCost;
+    private Switch activityIsPublic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = (Button) findViewById(R.id.createActivitySubmit);
+        activityName = (EditText) findViewById(R.id.editActivityName);
+        activityDescription = (EditText) findViewById(R.id.editActivityDescription);
+        activityStartTime = (EditText) findViewById(R.id.editActivityStartTime);
+        activityEndTime = (EditText) findViewById(R.id.editActivityEndTime);
+        activityTag = (EditText) findViewById(R.id.editActivityTag);
+        activityMaxPeople = (EditText) findViewById(R.id.editActivityPeople);
+        activityCost = (EditText) findViewById(R.id.editActivityCost);
+        activityLocation = (EditText) findViewById(R.id.editActivityLocation);
+        activityIsPublic = (Switch) findViewById(R.id.activityIsPublic);
+        //alertView = (TextView) findViewById(R.id.alertTextView);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("");
+                builder.setMessage("");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do something
+                    }
+                });
+                builder.show();
+                sumbitCreateActivity(view);
+            }
+
+        });
     }
 
     public void sumbitCreateActivity(View view)
     {
 
-        String eventName = ((EditText)findViewById(R.id.editActivityName)).getText().toString();
-        String eventDescription = ((EditText)findViewById(R.id.editActivityDescription)).getText().toString();
-        String eventStartTime = ((EditText)findViewById(R.id.editActivityStartTime)).getText().toString();
+        String activityName = ((EditText)findViewById(R.id.editActivityName)).getText().toString();
+        String activityDescription = ((EditText)findViewById(R.id.editActivityDescription)).getText().toString();
+        String activityStartTime = ((EditText)findViewById(R.id.editActivityStartTime)).getText().toString();
         Map<String, Object> activity = new HashMap<>();
-        activity.put("name", eventName);
-        activity.put("description", eventDescription);
-        activity.put("start_time", eventStartTime);
+        activity.put("name", activityName);
+        activity.put("description", activityDescription);
+        activity.put("start_time", activityStartTime);
 //
         // Add a new document with a generated ID
-        db.collection("event")
+        db.collection("activity")
                 .add(activity)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -117,6 +163,11 @@ public class MainActivity extends AppCompatActivity {
                         Log.w(TAG, "Error adding document", e);
                     }
                 });
+    }
+
+    public String checkAllFilled(){
+
+        return "Good";
     }
 
 }

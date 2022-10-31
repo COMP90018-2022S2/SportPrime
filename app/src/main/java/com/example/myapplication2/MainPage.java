6a9cf1bd4d79cc9d97f4b838a6343842d4fdcf51
Taelot <sdsqianxrfxdho@163.com>
2022-10-31 21:26:23 +1100

    merge page with other group members

--- app/src/main/java/com/example/myapplication2/MainPage.java
+++ app/src/main/java/com/example/myapplication2/MainPage.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ActivityMap extends AppCompatActivity {
-
-
+public class MainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
     }
 }

bd4a1f1078c47192768e00325d11d0c1049c8d9f
zsui0003 <zsui0003@student.monash.edu>
2022-10-31 22:09:38 +1100

    update mainpage component

--- app/src/main/java/com/example/myapplication2/MainPage.java
+++ app/src/main/java/com/example/myapplication2/MainPage.java
@@ -1,14 +1,48 @@
 package com.example.myapplication2;
 
 import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPage extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextView activityName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+        activityName = (TextView) findViewById(R.id.textView_test);
+        db.collection("activity")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
